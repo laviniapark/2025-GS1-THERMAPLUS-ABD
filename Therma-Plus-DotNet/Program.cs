47355162237fdf474e76f2df6bcabfe65fe64b49
BuildTools <unconfigured@null.spigotmc.org>
2025-08-29 13:57:32 -0300

    Revisao do codigo para corrigir erro

--- Therma-Plus-DotNet/Program.cs
+++ Therma-Plus-DotNet/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddAutoMapper(typeof(Program));
